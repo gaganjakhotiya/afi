0708c99c10664508f749ba741791639939fcd722
Gagan Jakhotiya <gagan@coverfoxmail.com>
2016-11-10 12:20:08 +0530

    lib bundle fix

--- lib/lib.tsx
+++ lib/lib.tsx
@@ -1,3 +1,17 @@
+import React = require('react')
+import ReactDOM = require('react-dom')
+import ReactRouter = require('react-router')
+import assign = require('object-assign')
+import EE = require('events')
+
 var req: any = require
 var requireContext = req.context('../lib', true, /\.tsx?$/)
 requireContext.keys().map(requireContext)
+
+export = {
+	React: React,
+	ReactDOM: ReactDOM,
+	ReactRouter: ReactRouter,
+	assign: assign,
+	EE: EE
+}
